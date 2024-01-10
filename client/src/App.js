b2f43021d2d7665265f836086d74b1b5afd2ee41
Luca Andreea <lucaandreearaluca@yahoo.com>
2024-01-10 21:29:09 +0200

    init client & server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p class="text-3xl font-bold underline">
+          Hello awdawdap!
+      </p>
+    </div>
+  );
+}
+
+export default App;
