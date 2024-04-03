50848498627a5aec024b43d06b9608f89eb27348
rursache <rursache@wiley.com>
2024-04-03 22:29:00 +0300

    refactor bar logic

--- client/src/components/bar/BarContainer.js
+++ client/src/components/bar/BarContainer.js
@@ -1,51 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import BarMenu from "./BarMenu";
 import BarSearchbar from "./BarSearchbar";
 import BarContent from "./BarContent";
 import PourButton from "./PourButton";
+import {
+    otherBeverages,
+    nonAlcoholicBeverages,
+    alcoholicBeverages,
+} from "../../utils/beverages";
 
+const barData = {
+    "Alcoholic": alcoholicBeverages,
+    "Non Alcoholic": nonAlcoholicBeverages,
+    "Other": otherBeverages
+}
 function BarContainer() {
+    const [menuOption, setMenuOption] = useState('Alcoholic');
+    const [filteredBarData, setFilteredBarData] = useState(barData[menuOption]);
 
-  const [barData, setBarData] = useState([]);
-  const [searchText, setSearchText] = useState("");
+    const [searchText, setSearchText] = useState("");
+    useEffect(() => {
 
-  useEffect(() => {
-    let filteredData = barData;
+        if (searchText === "") {
+            setFilteredBarData(barData[menuOption])
+        } else {
+            const filteredData = barData[menuOption].filter(item =>
+                item.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredBarData(filteredData);
+        }
+    }, [searchText]);
+    useEffect(() => {
+        setFilteredBarData(barData[menuOption])
+    },[menuOption])
+    console.log("RERENDER")
+    return (
+        <div className="flex flex-col h-full bg-gray-100 rounded-lg shadow-lg">
+            <div className="p-6 border-b border-gray-300">
+                <BarSearchbar
+                    sendSearchText={(searchText) => {
+                        setSearchText(searchText);
+                    }}
+                />
+            </div>
+            <div className="flex p-6 border-b border-gray-300">
+                <BarMenu menuOption={menuOption} setMenuOption={setMenuOption}/>
 
-    if (searchText) {
-      filteredData = barData.filter((item) =>
-        item.toLowerCase().includes(searchText.toLowerCase())
-      );
-    }
-  }, [searchText, barData]);
+                <BarContent barData={filteredBarData}/>
+            </div>
+            <PourButton/>
 
-  const filteredBarData = barData.filter((item) =>
-    item.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-
-  return (
-    <div className="flex flex-col h-full bg-gray-100 rounded-lg shadow-lg">
-      <div className="p-6 border-b border-gray-300">
-        <BarSearchbar
-          sendSearchText={(searchText) => {
-            setSearchText(searchText);
-          }}
-        />
-      </div>
-        <div className="flex p-6 border-b border-gray-300">
-            <BarMenu
-                sendBarData={(data) => {
-                    setBarData(data);
-                }}
-            />
-
-            <BarContent barData={filteredBarData}/>
         </div>
-        <PourButton/>
-
-    </div>
-  );
+    );
 }
 
 export default BarContainer;
--- client/src/components/bar/BarMenu.js
+++ client/src/components/bar/BarMenu.js
@@ -1,53 +1,24 @@
-import React, { useState, useEffect } from "react";
-import {
-  otherBeverages,
-  nonAlcoholicBeverages,
-  alcoholicBeverages,
-} from "../../utils/beverages";
-function BarMenu(props) {
+import React, { useState } from "react";
+
+function BarMenu({menuOption,setMenuOption}) {
   const barMenuItems = ["Alcoholic", "Non Alcoholic", "Other"];
-  const [menuOption, setMenuOption] = useState("Alcoholic");
-
-  const handleChooseOption = (event) => {
-    const chosenOption = event.target.textContent;
-    setMenuOption(chosenOption);
-  };
-
-  useEffect(() => {
-    switch (menuOption) {
-      case "Alcoholic":
-        props.sendBarData(alcoholicBeverages);
-        break;
-      case "Non Alcoholic":
-        props.sendBarData(nonAlcoholicBeverages);
-        break;
-      case "Other":
-        props.sendBarData(otherBeverages);
-        break;
-      default:
-        console.log("Default");
-        break;
-    }
-  }, [menuOption]);
-
+console.log('BarMenu rerender')
   return (
-    <div className="bg-gray-100 rounded-lg p-4">
+    <div className="bg-gray-100 w-48 rounded-lg p-4">
       <h1 className="text-lg font-semibold mb-2">Menu</h1>
-      <ul>
         {barMenuItems.map((item, index) => (
-          <li
+          <button
             key={index}
-            className={`cursor-pointer py-2 px-4 rounded-lg ${
+            className={`w-36 m-1 text-center cursor-pointer py-2 px-4 rounded-lg ${
               menuOption === item
                 ? "bg-blue-500 text-white"
                 : "hover:bg-gray-200"
             }`}
-            onClick={handleChooseOption}
+            onClick={() => setMenuOption(item)}
           >
             {item}
-          </li>
+          </button>
         ))}
-      </ul>
     </div>
   );
 }
